d8cbd2be77d38de4a7fa34191bc2f754c767cf37
pavel <jviktes@gmail.com>
2018-09-14 20:19:01 +0200

    prvni vypok

---
+++ WEB_server/Controllers/DefaultController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WEB_server.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index(string data)
+        {
+            ViewData["RFID"] = data;
+            return View("Index");
+        }
+    }
+}
